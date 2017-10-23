925585e51c0d1bcf201e17a56d486950c5658673
wang.zhongyi <meddkim@163.com>
2017-10-23 17:16:38 +0800

    first commit

---
+++ alienGame/settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """配置类"""
+    def __init__(self):
+        """游戏初始化设置"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
