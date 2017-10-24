a02ece15247abbca766073b1b201b41d36895742
wang.zhongyi <meddkim@163.com>
2017-10-24 18:59:09 +0800

    add bullet

--- alienGame/game_functions.py
+++ alienGame/game_functions.py
@@ -1,6 +1,19 @@
 import sys
 import pygame
 
+def check_keydown_events(event,ship):
+    """按键响应"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event,ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """"响应鼠标和键盘事件"""
     for event in pygame.event.get():
@@ -9,14 +22,12 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.rect.centerx += 1
-            elif event.key == pygame.K_LEFT:
-                ship.rect.centerx -= 1
+            check_keydown_events(event,ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event,ship)
 
 def update_screen(ai_settings,screen,ship):
     """"更新屏幕上的图像，并切换到新的屏幕上"""
     screen.fill(ai_settings.bg_color)
     ship.blitme()
-
     pygame.display.flip()
--- alienGame/ship.py
+++ alienGame/ship.py
@@ -1,9 +1,10 @@
 import pygame
-
+import settings as ai_settings
 class Ship():
     """飞船"""
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船并获取外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,5 +15,18 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        self.center = float(self.rect.centerx)
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
     def blitme(self):
         self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        """根据移动标志调整飞船位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
