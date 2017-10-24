e3c8cc447b37a77de581aed9c29447a5a14ac781
wang.zhongyi <meddkim@163.com>
2017-10-24 09:05:24 +0800

    let ship move

--- alienGame/game_functions.py
+++ alienGame/game_functions.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     """"响应鼠标和键盘事件"""
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             print(event.pos)
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.rect.centerx += 1
+            elif event.key == pygame.K_LEFT:
+                ship.rect.centerx -= 1
 
 def update_screen(ai_settings,screen,ship):
     """"更新屏幕上的图像，并切换到新的屏幕上"""
