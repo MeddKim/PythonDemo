16a38670095f57f7bcd460d4cf475dab30326c3b
unknown <meddkim@163.com>
2018-05-18 19:03:37 +0800

    fix

--- core/mail_tool/db/member.py
+++ core/mail_tool/db/member.py
@@ -1,21 +1,38 @@
 import re
 
 def requireInt(value,message):
-    if isinstance(value,int) != True:
+    if not isinstance(value,int):
         raise ValueError(value,message)
     return value
+
+def required(value,message):
+    if not value:
+        raise ValueError(value,message)
+    return value
+
 class Member(object):
     def __init__(self,id,name,email):
-        self.__id = id
-        self.__name = name
-        self.__email = email
+        self._id = id
+        self._name = name
+        self._email = email
 
     @property
     def id(self):
-        return self.__id
-
+        return self._id
     @id.setter
     def id(self,value):
-        self.__id = requireInt(value,"ID值必须为整数")
+        self._id = requireInt(value,"ID值必须为整数")
 
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self,value):
+        self._name = required(value,"名称为必输项")
 
+    @property
+    def email(self):
+        return self._name
+    @name.setter
+    def email(self,value):
+        self._email = value
